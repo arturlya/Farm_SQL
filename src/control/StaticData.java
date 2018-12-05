0eaa126bdc795627c2a2ad7c6462226881983f87
lyadartu <lyadskyyartur@gmail.com>
2018-12-05 11:51:55 +0100

    Semel

---
+++ src/control/StaticData.java
@@ -0,0 +1,13 @@
+package control;
+
+public class StaticData {
+
+    public static final String farm = "MarArtFarm_Farm";
+    public static final String pflanze = "MarArtFarm_Pflanze";
+    public static final String mitarbieter = "MarArtFarm_Mitarbeiter";
+    public static final String lager = "MarArtFarm_Lager";
+    public static final String tier = "MarArtFarm_Tier";
+    public static final String kunde = "MarArtFarm_Kunde";
+    public static final String shop = "MarArtFarm_Shop";
+
+}
