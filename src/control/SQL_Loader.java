57902a7abcc2256b6ff06bb426cdf4af139c727b
lyadartu <lyadskyyartur@gmail.com>
2018-12-14 10:17:33 +0100

    Richtiges Droppen

--- src/control/SQL_Loader.java
+++ src/control/SQL_Loader.java
@@ -17,7 +17,10 @@ public class SQL_Loader {
         try {
             Connection con = DriverManager.getConnection("jdbc:mysql://mysql.webhosting24.1blu.de/db85565x2810214?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "s85565_2810214", "kkgbeste");
             Statement stmt = con.createStatement();
-/*
+
+            try { stmt.execute("DROP TABLE "+StaticData.kuemmert_sich+";"); } catch (Exception e) { System.out.println("Tabelle Kümmert sich nicht gelöscht."); }
+            try { stmt.execute("DROP TABLE "+StaticData.mutationskammer+";"); } catch (Exception e) { System.out.println("Tabelle nicht gelöscht."); }
+            try { stmt.execute("DROP TABLE "+StaticData.wird_gekreuzt+";"); } catch (Exception e) { System.out.println("Tabelle nicht gelöscht."); }
             try { stmt.execute("DROP TABLE " + StaticData.shop + ";"); } catch (Exception e) { System.out.println("Tabelle Shop nicht gelöscht."); }
 
             try { stmt.execute("DROP TABLE " + StaticData.lager + ";"); } catch (Exception e) { System.out.println("Tabelle Lager nicht gelöscht."); }
@@ -25,7 +28,7 @@ public class SQL_Loader {
             try { stmt.execute("DROP TABLE " + StaticData.mitarbieter + ";"); } catch (Exception e) { System.out.println("Tabelle Mitarbeiter nicht gelöscht."); }
 
             try { stmt.execute("DROP TABLE " + StaticData.tier + ";"); } catch (Exception e) { System.out.println("Tabelle Tier nicht gelöscht."); }
-*/
+/*
             try{stmt.execute("DELETE FROM "+StaticData.tier+" WHERE tierID>0;");}catch (SQLException e){e.printStackTrace();}
 
             try{stmt.execute("DELETE FROM "+StaticData.shop+" WHERE kaufID>0;");}catch (SQLException e){e.printStackTrace();}
@@ -37,18 +40,29 @@ public class SQL_Loader {
             try{stmt.execute("DELETE FROM "+StaticData.farm+" WHERE farmID>0;");}catch (SQLException e){e.printStackTrace();}
 
             try{stmt.execute("DELETE FROM "+StaticData.mitarbieter+" WHERE mitarbeiterID>0;");}catch (SQLException e){e.printStackTrace();}
-/*
+*/
             try { stmt.execute("DROP TABLE " + StaticData.pflanze + ";"); } catch (Exception e) {
                 e.printStackTrace();
                 System.out.println("Tabelle Pflanze nicht gelöscht.");
             }
-            //try { stmt.execute("DROP TABLE "+StaticData.mutationskammer+";"); } catch (Exception e) { System.out.println("Tabelle nicht gelöscht."); }
-            //try { stmt.execute("DROP TABLE "+StaticData.wird_gekreuzt+";"); } catch (Exception e) { System.out.println("Tabelle nicht gelöscht."); }
+
             try { stmt.execute("DROP TABLE " + StaticData.farm + ";"); } catch (Exception e) {
                 e.printStackTrace();
                 System.out.println("Tabelle Farm nicht gelöscht.");
             }
 
+            try { stmt.execute("CREATE TABLE "+StaticData.farm+" (" +
+                    "farmID int NOT NULL AUTO_INCREMENT,"+
+                    "farmGeld double NOT NULL,"+
+                    "farmName varchar(255) NOT NULL,"+
+                    "farmFelder int NOT NULL,"+
+                    "PRIMARY KEY(farmID)"+
+                    // "FOREIGN KEY(shop) REFERENCES "+StaticData.shop+"(shopID)"+
+                    ");");
+            }catch (Exception e){
+                System.out.println("Keine neue Tabelle Farm angelegt");
+                //e.printStackTrace();
+            }
             try{ stmt.execute("CREATE TABLE "+StaticData.shop+" (" +
                     "kaufID int NOT NULL AUTO_INCREMENT,"+
                     "entitaet varchar(255) NOT NULL,"+
@@ -62,18 +76,7 @@ public class SQL_Loader {
                 e.printStackTrace();
                 //System.out.println("Keine neue Tabelle Shop angelegt.");
             }
-            try { stmt.execute("CREATE TABLE "+StaticData.farm+" (" +
-                        "farmID int NOT NULL AUTO_INCREMENT,"+
-                        "farmGeld double NOT NULL,"+
-                        "farmName varchar(255) NOT NULL,"+
-                        "farmFelder int NOT NULL,"+
-                        "PRIMARY KEY(farmID)"+
-                       // "FOREIGN KEY(shop) REFERENCES "+StaticData.shop+"(shopID)"+
-                        ");");
-            }catch (Exception e){
-                System.out.println("Keine neue Tabelle Farm angelegt");
-                //e.printStackTrace();
-            }
+
             try{ stmt.execute("CREATE TABLE "+StaticData.pflanze+" (" +
                     "pflanzenID int NOT NULL AUTO_INCREMENT,"+
                     "pflanzenArt varchar(255) NOT NULL,"+
@@ -115,13 +118,14 @@ public class SQL_Loader {
             try{ stmt.execute("CREATE TABLE "+StaticData.mitarbieter+" (" +
                     "mitarbeiterID int NOT NULL AUTO_INCREMENT,"+
                     "job varchar(255) NOT NULL,"+
-                    "farmID varchar(255) NOT NULL,"+
+                    "farmID int NOT NULL,"+
                     "lohn double NOT NULL,"+
                     "PRIMARY KEY(mitarbeiterID),"+
-                    "FOREIGN KEY(farmID) "+StaticData.farm+"(farmID)"+
+                    "FOREIGN KEY(farmID) REFERENCES "+StaticData.farm+"(farmID)"+
                     ");");
             }catch (Exception e){
-                System.out.println("Keine neue Tabelle Mitarbeiter angelegt.");
+                e.printStackTrace();
+                //System.out.println("Keine neue Tabelle Mitarbeiter angelegt.");
             }
             /*    stmt.execute("CREATE TABLE "+StaticData.mutationskammer+" (" +
                         "mutationskammerID int NOT NULL AUTO_INCREMENT,"+
